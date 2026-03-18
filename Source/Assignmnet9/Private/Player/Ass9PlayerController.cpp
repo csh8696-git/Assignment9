4bc957522f0d967134171b333637d88b1c039467
csh8696-git <csh8696@gmail.com>
2026-03-18 14:54:27 +0900

    add : controller-input
    컨트롤러와 인풋 관련 코드 추가

--- Source/Assignmnet9/Private/Player/Ass9PlayerController.cpp
+++ Source/Assignmnet9/Private/Player/Ass9PlayerController.cpp
@@ -1,20 +1,65 @@
 #include "Player/Ass9PlayerController.h"
+#include "Player/Ass9PlayerState.h"
+#include "Game/Ass9GameModeBase.h"
+#include "UI/Ass9ChatInput.h"
 #include "Assignmnet9.h"
 #include "Net/UnrealNetwork.h"
+#include "Kismet/GameplayStatics.h"
+#include "EngineUtils.h"
 
 AAss9PlayerController::AAss9PlayerController()
 {
 	bReplicates = true;
 }
 
-void AAss9PlayerController::PrintChatMessageString(const FString& InChatMessageString)
-{
-	Assignment9FunctionLibrary::MyPrintString(this, InChatMessageString, 10.f);
-}
 
 void AAss9PlayerController::ClientRPCPrintChatMessageString_Implementation(const FString& InChatMessageString)
 {
 	PrintChatMessageString(InChatMessageString);
+}
+
+void AAss9PlayerController::ServerRPCPrintChatMessageString_Implementation(const FString& InChatMessageString)
+{
+	AGameModeBase* GM = UGameplayStatics::GetGameMode(this);
+	if (IsValid(GM) == true)
+	{
+		AAss9GameModeBase* A9GM = Cast<AAss9GameModeBase>(GM);
+		if (IsValid(A9GM) == true)
+		{
+			A9GM->PrintChatMessageString(this, InChatMessageString);
+		}
+	}
+}
+
+void AAss9PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (IsLocalController() == false)
+	{
+		return;
+	}
+
+	FInputModeUIOnly InputModeUIOnly;
+	SetInputMode(InputModeUIOnly);
+
+	if (IsValid(ChatInputWidgetClass) == true)
+	{
+		ChatInputWidgetInstance = CreateWidget<UAss9ChatInput>(this, ChatInputWidgetClass);
+		if (IsValid(ChatInputWidgetInstance) == true)
+		{
+			ChatInputWidgetInstance->AddToViewport();
+		}
+	}
+
+	if (IsValid(NotificationTextWidgetClass) == true)
+	{
+		NotificationTextWidgetInstance = CreateWidget<UUserWidget>(this, NotificationTextWidgetClass);
+		if (IsValid(NotificationTextWidgetInstance) == true)
+		{
+			NotificationTextWidgetInstance->AddToViewport();
+		}
+	}
 }
 
 void AAss9PlayerController::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
@@ -22,4 +67,25 @@ void AAss9PlayerController::GetLifetimeReplicatedProps(TArray<class FLifetimePro
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
 	DOREPLIFETIME(ThisClass, NotificationText);
+}
+
+void AAss9PlayerController::SetChatMessageString(const FString& InChatMessageString)
+{
+	ChatMessageString = InChatMessageString;
+	if (IsLocalController() == true)
+	{
+		AAss9PlayerState* A9PS = GetPlayerState<AAss9PlayerState>();
+		if (IsValid(A9PS) == true)
+		{
+			FString CombinedMessageString = A9PS->GetPlayerInfoString()
+				+ TEXT(": ") + InChatMessageString;
+
+			ServerRPCPrintChatMessageString(CombinedMessageString);
+		}
+	}
+}
+
+void AAss9PlayerController::PrintChatMessageString(const FString& InChatMessageString)
+{
+	Assignment9FunctionLibrary::MyPrintString(this, InChatMessageString, 10.f);
 }
